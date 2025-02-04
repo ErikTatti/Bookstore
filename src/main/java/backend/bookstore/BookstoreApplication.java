663d28e44c1a71c8a1e54e4dd0a141b4f4baef88
bhm240@myy.haaga-helia.fi <bhm240@myy.haaga-helia.fi>
2025-02-04 10:49:24 +0200

    tehtava1

--- src/main/java/backend/bookstore/BookstoreApplication.java
+++ src/main/java/backend/bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package backend.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import backend.bookstore.domain.Book;
+import backend.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demoData(BookRepository repository) {
+		return (args) -> {
+
+			log.info("Save a couple of books");
+			repository.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929, "1234-5678", 12.99));
+			repository.save(new Book("Animal Farm", "George Orwell", 1945, "1234-5679", 9.99));
+			repository.save(new Book("To Kill a Mockingbird", "Harper Lee", 1960, "1234-5680", 10.99));
+			repository.save(new Book("1984", "George Orwell", 1949, "1234-5681", 11.99));
+			repository.save(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "1234-5682", 13.99));
+
+			log.info("tulostetaan kirjat");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
 	}
 
 }
--- src/main/java/backend/bookstore/domain/Book.java
+++ src/main/java/backend/bookstore/domain/Book.java
@@ -1,6 +1,17 @@
 package backend.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
 
     private String title;
     private String author;
@@ -8,7 +19,11 @@ public class Book {
     private String isbn;
     private double price;
 
-   
+    public Book() {
+        super();
+
+    }
+
     public Book(String title, String author, int publicationYear, String isbn, double price) {
         this.title = title;
         this.author = author;
@@ -17,7 +32,14 @@ public class Book {
         this.price = price;
     }
 
-  
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -63,4 +85,5 @@ public class Book {
         return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear
                 + ", isbn=" + isbn + ", price=" + price + "]";
     }
+
 }
